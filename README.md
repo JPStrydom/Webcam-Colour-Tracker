b4beb387de36c28d4c72a6ef0e3f94d330aec606
JP Strydom <jp.strydom@entelect.co.za>
2017-11-27 16:35:44 +0200

    Add HSV thresholding

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Webcam-Colour-Tracker
+
--- play.py
+++ play.py
@@ -14,26 +14,33 @@ detect_bool = False
 play_array = np.zeros([1, 2], dtype=tuple)
 img_draw = []
 
-tolerance = 0.15
+h_range = 5
+sv_range = 100
 
 
-def calculate_color_threshold(img_rgb):
-    img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2HSV)
+def calculate_color_threshold(img_brg):
+    img_hsv = cv2.cvtColor(img_brg, cv2.COLOR_BGR2HSV)
 
     scan_area = img_hsv[round(camera_height / 2) - 10: round(camera_height / 2) + 10,
                         round(camera_width / 2) - 10: round(camera_width / 2) + 10]
     avg_hsv_color = np.mean(np.mean(scan_area, axis=0), axis=0)
 
-    lower_threshold = np.array(avg_hsv_color * (1 - tolerance), dtype=int)
-    upper_threshold = np.array(avg_hsv_color * (1 + tolerance), dtype=int)
+    lower_threshold = np.array([avg_hsv_color[0] - h_range,
+                                avg_hsv_color[1] - sv_range,
+                                avg_hsv_color[2] - sv_range],
+                               dtype=int)
+    upper_threshold = np.array([avg_hsv_color[0] + h_range,
+                                avg_hsv_color[1] + sv_range,
+                                avg_hsv_color[2] + sv_range],
+                               dtype=int)
 
-    avg_rgb_color = tuple(cv2.cvtColor(np.uint8([[avg_hsv_color]]), cv2.COLOR_HSV2BGR)[0, 0])
+    avg_brg_color = tuple(cv2.cvtColor(np.uint8([[avg_hsv_color]]), cv2.COLOR_HSV2BGR)[0, 0])
 
-    return lower_threshold, upper_threshold, avg_rgb_color
+    return lower_threshold, upper_threshold, avg_brg_color
 
 
-def detect(img_rgb, lower_threshold, upper_threshold):
-    img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2HSV)
+def detect(img_brg, lower_threshold, upper_threshold):
+    img_hsv = cv2.cvtColor(img_brg, cv2.COLOR_BGR2HSV)
 
     mask = cv2.inRange(img_hsv, lower_threshold, upper_threshold)
 
